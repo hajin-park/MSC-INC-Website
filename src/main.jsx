eb4d55b1da427f48f4e51045d8cff08082656c5a
Woori <woori@wooris-mac-mini.lan>
2023-06-04 18:46:49 -0700

    Added React Router, page templates

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,41 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './routes/home.jsx'
+import Events from './routes/events.jsx'
+import About from './routes/about.jsx'
+import Contact from './routes/contact.jsx'
+import Donate from './routes/donate.jsx'
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/events",
+    element: <Events />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path: "/contact",
+    element: <Contact />,
+  },
+  {
+    path: "/donate",
+    element: <Donate />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
