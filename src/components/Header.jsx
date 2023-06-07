d26e112b38461485be79b0284e8207e44ab9cbd6
Woori <woori@wooris-mac-mini.lan>
2023-06-06 21:45:37 -0700

    Redesigned navigation bar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,28 +1,88 @@
-import { Link } from "react-router-dom"
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Dialog } from '@headlessui/react';
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
 import { useAuth } from '../authContext';
 
 export default function Header() {
     const { currentUser } = useAuth();
+    const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+    const navigation = [
+        { name: 'Home', to: 'home' },
+        { name: 'Events & Activities', to: 'events' },
+        { name: 'Our Story', to: 'story' },
+        { name: 'Contact', to: 'contact' },
+        { name: 'Donate', to: 'donate' }
+    ];
 
     return (
-        <nav className="sticky top-0 z-50 bg-sky-100 shadow-md">
-            <div className="relative flex items-center justify-between h-24">
-                <section className="flex items-center px-16">
-                    <div className="flex-shrink-0">
-                        <a href="/" className="text-4xl font-black text-blue-600 hover:text-blue-800 transition-colors">Merced Senior Citizens Inc.</a>
+        <header className="sticky top-0 z-50 bg-white shadow-md">
+            <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
+                <div className="flex lg:flex-1">
+                    <Link to="/" className="-m-1.5 p-1.5 text-3xl font-black text-blue-600 hover:text-blue-800 transition-colors">
+                        Merced Senior Citizens Inc.
+                    </Link>
+                </div>
+                <div className="flex lg:hidden">
+                    <button
+                        type="button"
+                        className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+                        onClick={() => setMobileMenuOpen(true)}
+                    >
+                        <span className="sr-only">Open main menu</span>
+                        <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+                    </button>
+                </div>
+                <div className="hidden lg:flex sm:gap-x-4 md:gap-x-12 lg:gap-x-24">
+                    {navigation.map((item) => (
+                        <Link key={item.name} to={item.to} className="text-lg font-semibold leading-6 text-gray-900">
+                            {item.name}
+                        </Link>
+                    ))}
+                </div>
+                <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+                    <Link key={currentUser ? "Account" : "Log In"} to="user-auth" className="text-lg font-semibold leading-6 text-gray-900">
+                        {currentUser ? "Account" : "Log In"} <span aria-hidden="true">&rarr;</span>
+                    </Link>
+                </div>
+            </nav>
+            <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
+                <div className="fixed inset-0 z-10" />
+                <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+                    <div className="flex items-center justify-between">
+                        <Link to="/" className="-m-1.5 p-1.5 text-4xl font-black text-blue-600 hover:text-blue-800 transition-colors">
+                            Merced Senior Citizens Inc.
+                        </Link>
+                        <button
+                            type="button"
+                            className="-m-2.5 rounded-md p-2.5 text-gray-700"
+                            onClick={() => setMobileMenuOpen(false)}
+                        >
+                            <span className="sr-only">Close menu</span>
+                            <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+                        </button>
                     </div>
-                </section>
-                <section className="flex-1 flex items-center justify-end px-16">
-                    <div className="flex space-x-8">
-                        <Link to={`home`} className="text-gray-900 inline-flex items-center px-1 pt-1 border-b-2 text-xl font-medium border-gray-400 hover:border-blue-600 transition-all">Home</Link>
-                        <Link to={`events`} className="text-gray-900 inline-flex items-center px-1 pt-1 border-b-2 text-xl font-medium border-gray-400 hover:border-blue-600 transition-all">Events & Activities</Link>
-                        <Link to={`story`} className="text-gray-900 inline-flex items-center px-1 pt-1 border-b-2 text-xl font-medium border-gray-400 hover:border-blue-600 transition-all">Our Story</Link>
-                        <Link to={`contact`} className="text-gray-900 inline-flex items-center px-1 pt-1 border-b-2 text-xl font-medium border-gray-400 hover:border-blue-600 transition-all">Contact</Link>
-                        <Link to={`donate`} className="text-gray-900 inline-flex items-center px-1 pt-1 border-b-2 text-xl font-medium border-gray-400 hover:border-blue-600 transition-all">Donate</Link>
-                        <Link to={`user-auth`} className="text-gray-900 inline-flex pl-8 items-center px-1 pt-1 border-l-2 text-xl font-medium border-gray-400 hover:text-blue-600 transition-all">{currentUser ? "Account" : "Log In"}</Link>
+                    <div className="mt-6 flow-root">
+                        <div className="-my-6 divide-y divide-gray-500/10">
+                            <div className="space-y-2 py-6">
+                                {navigation.map((item) => (
+                                    <Link
+                                        key={item.name}
+                                        to={item.to}
+                                        className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                                    >
+                                        {item.name}
+                                    </Link>
+                                ))}
+                                <Link key={currentUser ? "Account" : "Log In"} to="user-auth" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
+                                    {currentUser ? "Account" : "Log In"} <span aria-hidden="true">&rarr;</span>
+                                </Link>
+                            </div>
+                        </div>
                     </div>
-                </section>
-            </div>
-        </nav>
-    )
+                </Dialog.Panel>
+            </Dialog>
+        </header>
+    );
 }
