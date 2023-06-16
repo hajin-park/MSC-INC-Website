e12347c313267ebeaae5883a1526412315c26ae2
hajin-park <hajin1819@gmail.com>
2023-06-16 03:30:51 -0700

    Styled header photo gallery

--- src/components/HomePageHeaderSection.jsx
+++ src/components/HomePageHeaderSection.jsx
@@ -32,18 +32,15 @@ export default function HomePageHeaderSection() {
     };
 
     return (
-        <div className="w-full h-144 flex flex-col lg:flex-row items-center justify-center  bg-gray-100 text-gray-900">
-            <div className="w-full lg:w-1/2 h-1/2 gap-4 lg:h-full p-10 flex flex-col items-center justify-center text-center">
+            <div className="relative w-full h-144 p-16 lg:p-40 flex flex-col items-center justify-center text-center bg-gradient-to-b from-black/80 to-black/10 text-white">
+                <img src={`${images[currentImage]}`} alt="Homepage Gallery Image" className="absolute w-full h-full object-cover object-center mix-blend-overlay"/>
                 <h1 className="text-2xl lg:text-4xl font-bold">Welcome to Merced Senior Citizens Inc. & Merced Senior Community Center!</h1>
                 <p className="text-xl lg:text-2xl">Our mission is to provide seniors 50+ with meeting facilities and services specially designed to meet the physical, social and psychological needs and to promote the health, security, happiness, usefulness and longer life of the local senior community.</p>
-            </div>
-            <div className="w-full lg:w-1/2 h-1/2 lg:h-full flex items-center justify-center relative shadow-inner">
-                <button onClick={handlePrev} className="absolute left-0 top-1/2 transform -translate-y-1/2 z-10">
-                    <ChevronLeftIcon className="h-12 w-12 text-gray-500" />
+                <button onClick={handlePrev} className="absolute flex justify-start items-center w-1/3 h-full left-0 top-1/2 transform -translate-y-1/2 z-10 hover:bg-gradient-to-r from-black/50 to-black/0">
+                    <ChevronLeftIcon className="h-12 w-12 text-white" />
                 </button>
-                <img src={images[currentImage]} alt="" className="object-cover w-full h-full" />
-                <button onClick={handleNext} className="absolute right-0 top-1/2 transform -translate-y-1/2 z-10">
-                    <ChevronRightIcon className="h-12 w-12 text-gray-500" />
+                <button onClick={handleNext} className="absolute flex justify-end items-center w-1/3 h-full right-0 top-1/2 transform -translate-y-1/2 z-10 hover:bg-gradient-to-l from-black/50 to-black/0 ">
+                    <ChevronRightIcon className="h-12 w-12 text-white" />
                 </button>
                 <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 flex space-x-4 p-4">
                     {images.map((_, index) => (
@@ -51,6 +48,5 @@ export default function HomePageHeaderSection() {
                     ))}
                 </div>
             </div>
-        </div>
     );
 }
